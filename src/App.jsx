6ffbba8d8fc95ca2e00b57e86a0a81826da2cd8e
Chris Nielson <ctniel1@gmail.com>
2025-09-23 15:36:06 -0600

    Add Results table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
+import Results from "./components/Results"
 import Header from "./components/Header"
+import UserInput from "./components/UserInput"
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [inputIdentifier]: newValue,
+      }
+    })
+  }
+
   return (
     <>
       <Header />
-      <h1>React Investment Calculator</h1>
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
   )
 }
